1e06d2df79ad4b76003fb6d16dd0b2acc8546a01
Mahnoor Zahid <48029481+Mahnoor507@users.noreply.github.com>
2021-11-17 17:49:12 +0500

    Update train.py

--- train.py
+++ train.py
@@ -14,11 +14,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 import mlflow
 import mlflow.sklearn
+from mlflow.tracking import MlflowClient
 
 from urllib.parse import parse_qsl, urljoin, urlparse
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
+    client = MlflowClient()
     np.random.seed(40)
     df = pd.read_csv ('heart.csv')
     X = df.drop(['target'], axis=1)
@@ -32,28 +34,32 @@ if __name__ == "__main__":
 
     #mlflow.create_experiment("Mlflow Practice Experiments")
     with mlflow.start_run(run_name="Mlflow Practice") as run:
-        run_id = run.info.run_id
-        experiment_id = run.info.experiment_id
-        model=RandomForestClassifier(n_estimators=c)
-   
-        model.fit(x_train,y_train)
-        y_pred = model.predict(x_test)
-        accuracy = model.score(x_test,y_test)
-        recall=recall_score(y_test, y_pred)
-        f1=f1_score(y_test,y_pred)
-        precision=precision_score(y_test,y_pred)
-        print(accuracy)
-
-        mlflow.log_param("n_estimators", c)
-
-        mlflow.log_metric("accuracy", accuracy)
-        mlflow.log_metric("recall", recall)
-        mlflow.log_metric("f1 score", f1)
-        mlflow.log_metric("precision", precision)
-        print(run_id,experiment_id)
-
-
-        mlflow.sklearn.log_model(model, "model")
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-  
+      flag=False;
+      model=RandomForestClassifier(n_estimators=c)
+      model.fit(x_train,y_train)
+      y_pred = model.predict(x_test)
+      accuracy = model.score(x_test,y_test)
+      recall=recall_score(y_test, y_pred)
+      f1=f1_score(y_test,y_pred)
+      precision=precision_score(y_test,y_pred)
+      print(accuracy)
+      mlflow.log_param("n_estimators", c)
+      mlflow.log_metric("accuracy", accuracy)
+      mlflow.log_metric("recall", recall)
+      mlflow.log_metric("f1 score", f1)
+      mlflow.log_metric("precision", precision)     
+      mlflow.sklearn.log_model(model, "model")
+      filter_string="name = 'Random Forest Model'"
+      tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+      new=client.search_registered_models(filter_string=filter_string)
+      if(new):
+        id=new[0]._latest_version[0].run_id  
+        ok=client.get_metric_history(run_id=id,key="accuracy")
+        version_accuracy=float(ok[0].value)
+        if (version_accuracy>accuracy):
+          if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(model, "model")
+            flag = True;
+      if flag==False:
+        mlflow.sklearn.log_model(model, "model", registered_model_name="Random Forest Model")
 
